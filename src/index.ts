68cb1fb6f80134c12916bd83c91b5641b6e7c2dd
maulanafadilah <maulanafadilah028@gmail.com>
2025-04-22 10:54:46 +0700

    set up workspace

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-// Reexport the native module. On web, it will be resolved to ExpoStoreUpdatesModule.web.ts
-// and on native platforms to ExpoStoreUpdatesModule.ts
-export { default } from './ExpoStoreUpdatesModule';
-export { default as ExpoStoreUpdatesView } from './ExpoStoreUpdatesView';
-export * from  './ExpoStoreUpdates.types';
+import ExpoStoreUpdatesModule from "./ExpoStoreUpdatesModule";
+
+export function hello(): string {
+  return ExpoStoreUpdatesModule.hello();
+}
